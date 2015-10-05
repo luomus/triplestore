a3ae2c7acab8ca88403c6040578b7d1065249ba6
eopiirai <esko.piirainen@helsinki.fi>
2015-10-05 13:42:31 +0300

    it-viewer: ei cachesta

--- WEB-INF/src/main/fi/luomus/triplestore/service/InformationSystemsViewerServlet.java
+++ WEB-INF/src/main/fi/luomus/triplestore/service/InformationSystemsViewerServlet.java
@@ -2,8 +2,6 @@ package fi.luomus.triplestore.service;
 
 import fi.luomus.commons.containers.rdf.Statement;
 import fi.luomus.commons.services.ResponseData;
-import fi.luomus.commons.utils.SingleObjectCacheResourceInjected;
-import fi.luomus.commons.utils.SingleObjectCacheResourceInjected.CacheLoader;
 import fi.luomus.triplestore.dao.TriplestoreDAO;
 import fi.luomus.triplestore.models.Model;
 
@@ -36,87 +34,81 @@ public class InformationSystemsViewerServlet extends EditorBaseServlet {
 		if ("dependency-graph".equals(req.getParameter("view"))) responseData.setViewName("it-dependency-graph");
 
 		TriplestoreDAO dao = getTriplestoreDAO();
-		
-		Map<String, List<Model>> systems = SYSTEMS_CACHE.get(dao);
-		
+
+		Map<String, List<Model>> systems = loadSystems(dao);
+
 		int activeCount = 0;
 		for (List<Model> list : systems.values()) {
 			activeCount += list.size();
 		}
 		activeCount -= systems.get(Sixfold.ABANDONED.toString()).size();
-		
-//		Model system = systems.get("").get(0);
-//		system.getStatements("").iterator().next().getObjectLiteral().get
-//		
+
+		//		Model system = systems.get("").get(0);
+		//		system.getStatements("").iterator().next().getObjectLiteral().get
+		//		
 		//dao.getProperties("").getAllProperties().iterator().next().getRange().getValueFor("")
-//		
+		//		
 		responseData.setData("properties", dao.getProperties("KE.informationSystem"));
 		responseData.setData("systems", systems);
 		responseData.setData("activeCount", activeCount);
-		
+
 		return responseData;
 	}
 
-	private static final SingleObjectCacheResourceInjected<Map<String, List<Model>>, TriplestoreDAO> SYSTEMS_CACHE = 
-			new SingleObjectCacheResourceInjected<Map<String, List<Model>>, TriplestoreDAO>(new SystemsLoader(), 60*3); 
-
-	private static class SystemsLoader implements CacheLoader<Map<String, List<Model>>, TriplestoreDAO> {
-
-		@Override
-		public Map<String, List<Model>> load(TriplestoreDAO dao) {
-			try {
-				return tryToLoad(dao);
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		}
-
-		private Map<String, List<Model>> tryToLoad(TriplestoreDAO dao) throws Exception {
-			Map<String, List<Model>> systems = new HashMap<>();
-			for (Model system : dao.getSearchDAO().search("rdf:type", "KE.informationSystem")) {
-				String state = getState(system);
-				String publicity = getPublicity(system);
-				String slot = resolveSixfoldSlot(state, publicity).toString();
-				if (!systems.containsKey(slot)) {
-					systems.put(slot, new ArrayList<Model>());
-				}
-				systems.get(slot).add(system);
-			}
-			return systems;
-		}
-
-		private String getPublicity(Model system) {
-			List<Statement> publicities = system.getStatements("KE.publicity");
-			if (publicities.isEmpty()) return "";
-			if (publicities.size() > 1) throw new IllegalStateException("Too many publicities for " + system.getSubject().getQname());
-			return publicities.get(0).getObjectResource().getQname();
-		}
-
-		private String getState(Model system) {
-			List<Statement> states = system.getStatements("KE.state");
-			if (states.isEmpty()) return "";
-			if (states.size() > 1) throw new IllegalStateException("Too many states for " + system.getSubject().getQname());
-			return states.get(0).getObjectResource().getQname();
-		}
-
-		private Sixfold resolveSixfoldSlot(String state, String publicity) {
-			if (state.equals("KE.abandonedState")) 
-				return Sixfold.ABANDONED;
-			if (publicity.equals("KE.adminOnly")) 
-				return Sixfold.ADMIN;
-			if (state.equals("KE.productionState") && publicity.equals("KE.inPublicUse")) {
-				return Sixfold.PUBLIC_PRODUCTION;
-			}
-			if (state.equals("KE.productionState") && publicity.equals("KE.inUseByLuomus")) {
-				return Sixfold.INTERNAL_PRODUCTION;
-			}
-			if (state.equals("KE.developmentState") && publicity.equals("KE.inPublicUse")) {
-				return Sixfold.PUBLIC_DEVELOPMENT;
-			}
-			if (state.equals("KE.developmentState") && publicity.equals("KE.inUseByLuomus")) {
-				return Sixfold.INTERNAL_DEVELOPMENT;
-			}
-			return Sixfold.UKNOWN;
+	Map<String, List<Model>> loadSystems(TriplestoreDAO dao) {
+		try {
+			return tryToLoad(dao);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
 	}
+
+	private Map<String, List<Model>> tryToLoad(TriplestoreDAO dao) throws Exception {
+		Map<String, List<Model>> systems = new HashMap<>();
+		for (Model system : dao.getSearchDAO().search("rdf:type", "KE.informationSystem")) {
+			String state = getState(system);
+			String publicity = getPublicity(system);
+			String slot = resolveSixfoldSlot(state, publicity).toString();
+			if (!systems.containsKey(slot)) {
+				systems.put(slot, new ArrayList<Model>());
+			}
+			systems.get(slot).add(system);
+		}
+		return systems;
+	}
+
+	private String getPublicity(Model system) {
+		List<Statement> publicities = system.getStatements("KE.publicity");
+		if (publicities.isEmpty()) return "";
+		if (publicities.size() > 1) throw new IllegalStateException("Too many publicities for " + system.getSubject().getQname());
+		return publicities.get(0).getObjectResource().getQname();
+	}
+
+	private String getState(Model system) {
+		List<Statement> states = system.getStatements("KE.state");
+		if (states.isEmpty()) return "";
+		if (states.size() > 1) throw new IllegalStateException("Too many states for " + system.getSubject().getQname());
+		return states.get(0).getObjectResource().getQname();
+	}
+
+	private Sixfold resolveSixfoldSlot(String state, String publicity) {
+		if (state.equals("KE.abandonedState")) 
+			return Sixfold.ABANDONED;
+		if (publicity.equals("KE.adminOnly")) 
+			return Sixfold.ADMIN;
+		if (state.equals("KE.productionState") && publicity.equals("KE.inPublicUse")) {
+			return Sixfold.PUBLIC_PRODUCTION;
+		}
+		if (state.equals("KE.productionState") && publicity.equals("KE.inUseByLuomus")) {
+			return Sixfold.INTERNAL_PRODUCTION;
+		}
+		if (state.equals("KE.developmentState") && publicity.equals("KE.inPublicUse")) {
+			return Sixfold.PUBLIC_DEVELOPMENT;
+		}
+		if (state.equals("KE.developmentState") && publicity.equals("KE.inUseByLuomus")) {
+			return Sixfold.INTERNAL_DEVELOPMENT;
+		}
+		return Sixfold.UKNOWN;
+	}
+
 }
