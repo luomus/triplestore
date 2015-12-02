fea8dc8f493c6018705652323506b4b375211fae
eopiirai <esko.piirainen@helsinki.fi>
2015-12-02 14:03:32 +0200

    statement cache

--- WEB-INF/src/main/fi/luomus/triplestore/dao/DataSourceDefinition.java
+++ WEB-INF/src/main/fi/luomus/triplestore/dao/DataSourceDefinition.java
@@ -25,7 +25,7 @@ public class DataSourceDefinition {
 		p.setValidationInterval(60 * 1000); // 60 seconds
 		p.setJdbcInterceptors(
 				"org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;"+
-				"org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer");
+				"org.apache.tomcat.jdbc.pool.interceptor.StatementCache");
 		
 		DataSource datasource = new DataSource();
 		datasource.setPoolProperties(p);
