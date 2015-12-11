c6555d2e46a9bf3003991121cd893fcfb044edcd
eopiirai <esko.piirainen@helsinki.fi>
2015-12-11 08:48:06 +0200

    fix: laji-auth etc urls not needed from config for taxonomyeditor

--- WEB-INF/src/main/fi/luomus/triplestore/service/EditorBaseServlet.java
+++ WEB-INF/src/main/fi/luomus/triplestore/service/EditorBaseServlet.java
@@ -82,9 +82,11 @@ public abstract class EditorBaseServlet extends BaseServlet {
 		}
 		Config config = getConfig();
 
-		responseData.setData("taxonomyEditorBaseURL", config.get("taxonomyEditorBaseURL"));
-		responseData.setData("lajiETLBaseURL", config.get("lajiETLBaseURL"));
-		responseData.setData("lajiAuthBaseURL", config.get("lajiAuthBaseURL"));
+		if (config.defines("taxonomyEditorBaseURL")) {
+			responseData.setData("taxonomyEditorBaseURL", config.get("taxonomyEditorBaseURL"));
+			responseData.setData("lajiETLBaseURL", config.get("lajiETLBaseURL"));
+			responseData.setData("lajiAuthBaseURL", config.get("lajiAuthBaseURL"));
+		}
 
 		return responseData;
 	}
