c5deebe9c025e1d28cf977ebfe59a9b8cb62d0d8
eopiirai <esko.piirainen@helsinki.fi>
2016-06-07 01:02:56 +0300

    nop refact

--- WEB-INF/src/taxonomy/main/fi/luomus/triplestore/taxonomy/service/IUCNEvaluationEditServlet.java
+++ WEB-INF/src/taxonomy/main/fi/luomus/triplestore/taxonomy/service/IUCNEvaluationEditServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = {"/taxonomy-editor/iucn/species/*"})
-public class IUCNSpeciesEditServlet extends IUCNFrontpageServlet {
+public class IUCNEvaluationEditServlet extends IUCNFrontpageServlet {
 
 	private static final long serialVersionUID = -9070670126541614377L;
 
