a4c201574f51c0008f9994f094c72c9368f0d80c
eopiirai <esko.piirainen@helsinki.fi>
2016-06-06 11:25:28 +0300

    lataillaan evaluation datat ja statsit sen perusteella

--- WEB-INF/src/taxonomy/main/fi/luomus/triplestore/taxonomy/models/TaxonGroupIucnEvaluationData.java
+++ WEB-INF/src/taxonomy/main/fi/luomus/triplestore/taxonomy/models/TaxonGroupIucnEvaluationData.java
@@ -1,14 +1,18 @@
 package fi.luomus.triplestore.taxonomy.models;
 
+import fi.luomus.commons.containers.rdf.Model;
 import fi.luomus.commons.containers.rdf.Qname;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TaxonGroupIucnEvaluationData {
 
 	private final Qname groupQname;
 	private final Collection<String> speciesOfGroup;
-	
+	private final Map<Integer, EvaluationYear> years = new HashMap<>();
+
 	public TaxonGroupIucnEvaluationData(Qname groupQname, Collection<String> speciesOfGroup) {
 		this.groupQname = groupQname;
 		this.speciesOfGroup = speciesOfGroup;
@@ -23,18 +27,43 @@ public class TaxonGroupIucnEvaluationData {
 	}
 
 	public EvaluationYear getYear(int year) {
-		return new EvaluationYear(year);
+		if (!years.containsKey(years)) {
+			years.put(year, new EvaluationYear(year, this));
+		}
+		return years.get(year);
 	}
-	
+
 	public static class EvaluationYear {
 		private final int year;
-		public EvaluationYear(int year) {
+		private final TaxonGroupIucnEvaluationData baseData;
+		private final Map<String, Model> speciesEvaluations = new HashMap<>();
+		private Integer readyCount = null;
+
+		public EvaluationYear(int year, TaxonGroupIucnEvaluationData taxonGroupIucnEvaluationData) {
 			this.year = year;
+			this.baseData = taxonGroupIucnEvaluationData;
 		}
 		public int getReadyCount() {
-			return 480;
+			if (readyCount != null) return readyCount;
+			readyCount = countReady();
+			return readyCount;
 		}
+		private int countReady() {
+			int count = 0;
+			for (Model evaluation : speciesEvaluations.values()) {
+				if (evaluation.getStatements("MKV.state").get(0).getObjectResource().getQname().equals("MKV.stateReady")) {
+					count++;
+				}
+			}
+			return count;
+		}		
+		public EvaluationYear setEvaluation(String speciesQname, Model evaluation) {
+			readyCount = null;
+			speciesEvaluations.put(speciesQname, evaluation);
+			return this;
+		}
+
 	}
-	
+
 }
 
--- WEB-INF/src/taxonomy/main/fi/luomus/triplestore/taxonomy/service/ApiIucnDataServlet.java
+++ WEB-INF/src/taxonomy/main/fi/luomus/triplestore/taxonomy/service/ApiIucnDataServlet.java
@@ -9,6 +9,7 @@ import fi.luomus.triplestore.dao.SearchParams;
 import fi.luomus.triplestore.taxonomy.models.TaxonGroupIucnEvaluationData;
 
 import java.net.URI;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
@@ -50,6 +51,39 @@ public class ApiIucnDataServlet extends ApiBaseServlet {
 	}
 
 	private TaxonGroupIucnEvaluationData loadGroupData(String groupQname) throws Exception {
+		Set<String> speciesOfGroup = loadSpeciesOfGroup(groupQname);
+		TaxonGroupIucnEvaluationData data = new TaxonGroupIucnEvaluationData(new Qname(groupQname), speciesOfGroup);
+		addEvaluations(speciesOfGroup, data);
+		return data;
+	}
+
+	private void addEvaluations(Set<String> speciesOfGroup, TaxonGroupIucnEvaluationData data) throws Exception {
+		for (String speciesQname : speciesOfGroup) {
+			addEvaluations(data, speciesQname);
+		}
+	}
+
+	private void addEvaluations(TaxonGroupIucnEvaluationData data, String speciesQname) throws Exception {
+		Collection<Model> evaluations = getEvaluations(speciesQname);
+		for (Model evaluation : evaluations) {
+			int year = getEvaluationYear(evaluation);
+			data.getYear(year).setEvaluation(speciesQname, evaluation);
+		}
+	}
+
+	private int getEvaluationYear(Model evaluation) {
+		return Integer.valueOf(evaluation.getStatements("MKV.evaluationYear").get(0).getObjectLiteral().getContent());
+	}
+
+	private Collection<Model> getEvaluations(String speciesQname) throws Exception {
+		return getTriplestoreDAO().getSearchDAO().search(
+				new SearchParams(Integer.MAX_VALUE, 0)
+				.type("MKV.iucnRedListEvaluation")
+				.predicate("MKV.evaluatedTaxon")
+				.objectresource(speciesQname));
+	}
+
+	private Set<String> loadSpeciesOfGroup(String groupQname) throws Exception {
 		Set<String> rootTaxonsOfGroup = getRootTaxonsOfGroup(groupQname);
 		Set<String> speciesOfGroup = new LinkedHashSet<>();
 		HttpClientService client = null;
@@ -61,7 +95,7 @@ public class ApiIucnDataServlet extends ApiBaseServlet {
 		} finally {
 			if (client !=  null) client.close();
 		}
-		return new TaxonGroupIucnEvaluationData(new Qname(groupQname), speciesOfGroup);
+		return speciesOfGroup;
 	}
 
 	private Set<String> getRootTaxonsOfGroup(String groupQname) throws Exception {
