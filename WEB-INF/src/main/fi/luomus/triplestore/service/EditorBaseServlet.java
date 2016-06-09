2dbae6c70d9266e9a642c155e64dbd7a048b0022
eopiirai <esko.piirainen@helsinki.fi>
2016-06-09 15:29:08 +0300

    stagingilla apache syö original urlin; katotaan toimisko tämä

--- WEB-INF/src/main/fi/luomus/triplestore/service/EditorBaseServlet.java
+++ WEB-INF/src/main/fi/luomus/triplestore/service/EditorBaseServlet.java
@@ -54,39 +54,29 @@ public abstract class EditorBaseServlet extends BaseServlet {
 	protected ResponseData notAuthorizedRequest(HttpServletRequest req, HttpServletResponse res) {
 		log(req);
 		URIBuilder uri = new URIBuilder(getConfig().get("LoginURL"));
-		uri.addParameter("next", getURL(req).replace(getConfig().baseURL(), ""));
+		String originalURL = getURL(req);
+		String next = originalURL.replace("/triplestore", "").replace("/taxonomy-editor", "");
+		uri.addParameter("next", next);
 		return redirectTo(uri.toString(), res);
 	}
 
 	public static String getURL(HttpServletRequest req) {
+		String servletPath = req.getServletPath();   // /servlet/MyServlet
+		String pathInfo = req.getPathInfo();         // /a/b;c=123
+		String queryString = req.getQueryString();   // d=789
 
-	    String scheme = req.getScheme();             // http
-	    String serverName = req.getServerName();     // hostname.com
-	    int serverPort = req.getServerPort();        // 80
-	    String contextPath = req.getContextPath();   // /mywebapp
-	    String servletPath = req.getServletPath();   // /servlet/MyServlet
-	    String pathInfo = req.getPathInfo();         // /a/b;c=123
-	    String queryString = req.getQueryString();   // d=789
-
-	    // Reconstruct original requesting URL
-	    StringBuilder url = new StringBuilder();
-	    url.append(scheme).append("://").append(serverName);
-
-	    if (serverPort != 80 && serverPort != 443) {
-	        url.append(":").append(serverPort);
-	    }
-
-	    url.append(contextPath).append(servletPath);
-
-	    if (pathInfo != null) {
-	        url.append(pathInfo);
-	    }
-	    if (queryString != null) {
-	        url.append("?").append(queryString);
-	    }
-	    return url.toString();
+		// Reconstruct original requesting URL
+		StringBuilder url = new StringBuilder();
+		url.append(servletPath);
+		if (pathInfo != null) {
+			url.append(pathInfo);
+		}
+		if (queryString != null) {
+			url.append("?").append(queryString);
+		}
+		return url.toString();
 	}
-	
+
 	protected static final List<CreatableResource> CREATABLE_RESOURCES = Utils.list(
 			new CreatableResource("MA", "Person", "MA.person"),
 			new CreatableResource("KE", "Information System", "KE.informationSystem"),
@@ -193,7 +183,7 @@ public abstract class EditorBaseServlet extends BaseServlet {
 		}
 		return true;
 	}
-	
+
 	@Override
 	protected SessionHandler getSession(HttpServletRequest req) {
 		SessionHandler sessionHandler = new SessionHandlerImple(req.getSession(true), "triplestore");
