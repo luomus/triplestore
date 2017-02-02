56c424166ec7cd526c14e4ed1f694af56aada4db
eopiirai <esko.piirainen@helsinki.fi>
2017-02-02 18:58:20 +0200

    unohtunut add

---
+++ WEB-INF/src/taxonomy/main/fi/luomus/triplestore/taxonomy/iucn/model/Remark.java
@@ -0,0 +1,67 @@
+package fi.luomus.triplestore.taxonomy.iucn.model;
+
+public class Remark implements Comparable<Remark> {
+
+	private final int id;
+	private final IUCNEvaluationTarget target;
+	private final String date;
+	private final String remark;
+	private final String personName;
+	
+	public Remark(int id, IUCNEvaluationTarget target, String date, String remark, String personName) {
+		this.id = id;
+		this.target = target;
+		this.date = date;
+		this.remark = remark;
+		this.personName = personName;
+		if (id <= 0) throw new IllegalStateException("Id is " + id);
+	}
+
+	public IUCNEvaluationTarget getTarget() {
+		return target;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public String getShortenedRemark() {
+		if (remark.length() > 40) {
+			return remark.substring(0, 39) + "...";
+		}
+		return remark;
+	}
+	
+	public String getPersonName() {
+		return personName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public int compareTo(Remark o) {
+		return Integer.valueOf(id).compareTo(o.getId());
+	}
+
+	@Override
+	public int hashCode() {
+		return id;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Remark other = (Remark) obj;
+		if (id != other.id) return false;
+		return true;
+	}
+		
+}
