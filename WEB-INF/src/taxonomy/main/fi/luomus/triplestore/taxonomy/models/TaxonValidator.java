55e35223b4c8e39284f057a21608f8ac19b0f2f7
esko-piirainen <esko.piirainen@helsinki.fi>
2017-05-24 16:57:24 +0300

    sallitaan ë -merkki

--- WEB-INF/src/taxonomy/main/fi/luomus/triplestore/taxonomy/models/TaxonValidator.java
+++ WEB-INF/src/taxonomy/main/fi/luomus/triplestore/taxonomy/models/TaxonValidator.java
@@ -110,7 +110,7 @@ public class TaxonValidator {
 
 	private static final Set<Character> ALPHAS = Utils.set(
 			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
-			'w', 'x', 'y', 'z', 'å', 'ä', 'ö', 'é', 'ü', 'æ', 'í');
+			'w', 'x', 'y', 'z', 'å', 'ä', 'ö', 'é', 'ü', 'æ', 'í', 'ë');
 
 	private static final Set<Character> VERNACULAR_ALLOWED = Utils.set('-', ' ');
 
