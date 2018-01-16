b306cdc60567405ae14fd1f48ec1e34776e29e05
esko-piirainen <esko.piirainen@helsinki.fi>
2018-01-16 15:00:18 +0200

    new index for taxon search

--- sql/rdf_taxon_search.sql
+++ sql/rdf_taxon_search.sql
@@ -62,7 +62,11 @@ CREATE MATERIALIZED VIEW "LTKM_LUONTO"."TAXON_SEARCH_MATERIALIZED" ("CHECKLIST",
 );
 COMMENT ON MATERIALIZED VIEW "LTKM_LUONTO"."TAXON_SEARCH_MATERIALIZED" IS 'snapshot table for snapshot LTKM_LUONTO.TAXON_SEARCH_MATERIALIZED';
     
-create index ix_taxon_search_checklistname on taxon_search_materialized (checklist, name);
+CREATE INDEX ix_taxon_search ON taxon_search_materialized (checklist, name);
+CREATE INDEX ix_taxon_search_reverse ON taxon_search_materialized (checklist, REVERSE(name));
+CREATE INDEX ix_taxon_search_2 ON taxon_search_materialized (COALESCE(checklist, '.'), name);
+CREATE INDEX ix_taxon_search_reverse_2 ON taxon_search_materialized (COALESCE(checklist, '.'), REVERSE(name));
+
 grant select on TAXON_SEARCH_MATERIALIZED to ltkm_api;
 grant select on TAXON_SEARCH_MATERIALIZED to ltkm_taxonviewer;
 grant select on TAXON_SEARCH_MATERIALIZED to LTKM_KOTKA;
