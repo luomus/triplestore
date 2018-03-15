1b01b655a67ab903b9a5e2aa5e21685a600a6d59
esko-piirainen <esko.piirainen@helsinki.fi>
2018-03-15 12:58:59 +0200

    tämä on unohtunut ecplise asetusten kadotuksen takia addata

---
+++ WEB-INF/src/taxonomy/main/fi/luomus/triplestore/taxonomy/service/ApiHideUnhideTaxon.java
@@ -0,0 +1,45 @@
+package fi.luomus.triplestore.taxonomy.service;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fi.luomus.commons.containers.rdf.Predicate;
+import fi.luomus.commons.containers.rdf.Qname;
+import fi.luomus.commons.containers.rdf.Statement;
+import fi.luomus.commons.containers.rdf.Subject;
+import fi.luomus.commons.services.ResponseData;
+import fi.luomus.triplestore.dao.TriplestoreDAO;
+import fi.luomus.triplestore.taxonomy.models.EditableTaxon;
+
+@WebServlet(urlPatterns = {"/taxonomy-editor/api/hideTaxon/*", "/taxonomy-editor/api/unhideTaxon/*"})
+public class ApiHideUnhideTaxon extends ApiBaseServlet {
+
+	private static final long serialVersionUID = 8057985427638993882L;
+	private static final Predicate HIDDEN_TAXON = new Predicate("MX.hiddenTaxon");
+
+	@Override
+	protected ResponseData processPost(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		String taxonQname = getQname(req);
+		if (!given(taxonQname)) {
+			return redirectTo500(res);
+		} 
+		if (!taxonQname.contains(".")) taxonQname = taxonQname.replace("MX", "MX.");
+
+		EditableTaxon taxon = (EditableTaxon) getTaxonomyDAO().getTaxon(new Qname(taxonQname));
+		checkPermissionsToAlterTaxon(taxon, req);
+
+		TriplestoreDAO dao = getTriplestoreDAO();
+		Subject subject = new Subject(taxonQname);
+		if (req.getRequestURI().contains("/hideTaxon/")) {
+			dao.store(subject, new Statement(HIDDEN_TAXON, true));
+		} else {
+			dao.delete(subject, HIDDEN_TAXON);
+		}
+		
+		taxon.invalidateSelf();
+
+		return apiSuccessResponse(res);
+	}
+
+}
